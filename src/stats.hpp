92c0c2835f50e1b8133bcdc830fde3bc42f8de98
kataklinger <kataklinger@gmail.com>
2022-08-09 21:40:53 +0200

    Constraining stats computation callbacks

--- src/stats.hpp
+++ src/stats.hpp
@@ -208,32 +208,40 @@ namespace stats {
     };
   };
 
-  template<typename Tag>
-  struct generic_counter {
+  template<std::semiregular Value, typename Tag>
+  struct generic_value {
     template<typename Population>
     class type {
+    public:
+      using value_t = Value;
+      using tag_t = Tag;
+
     public:
       inline type(Population const& population, type const& previous) noexcept {
       }
 
-      inline auto count_value() const noexcept {
-        return count_;
+      inline auto generic_value() const noexcept {
+        return value_;
       }
 
-      inline void set_count_value(std::size_t count) noexcept {
-        count_ = count;
+      inline void set_generic_value(value_t value) noexcept {
+        value_ = value;
       }
 
     private:
-      std::size_t count_;
+      value_t value_;
     };
   };
+
+  template<typename Tag>
+  using generic_counter = generic_value<std::size_t, Tag>;
 
   template<typename Tag>
   struct generic_timer {
     template<typename Population>
     class type {
     public:
+      using tag_t = Tag;
       using timer_t = std::chrono::high_resolution_clock;
 
     public:
@@ -537,25 +545,76 @@ namespace stats {
     }
   }
 
-  template<typename Tag, typename Statistics, typename Fn>
-  inline void
-      count_simple(Statistics& statistics,
-                   Fn&& fn) requires(std::is_invocable_r_v<std::size_t, Fn>) {
-    using counter_t = generic_counter<Tag>;
-
-    if constexpr (tracks_statistic_v<Statistics, counter_t>) {
-      statistics.get<counter_t>().set_count_value(
-          std::invoke(std::forward<Fn>(fn)));
-    }
-  }
-
   template<typename Tag, typename Statistics, std::ranges::sized_range Range>
   inline void count_range(Statistics& statistics, Range const& range) {
     using counter_t = generic_counter<Tag>;
 
     if constexpr (tracks_statistic_v<Statistics, counter_t>) {
-      statistics.get<counter_t>().set_count_value(std::ranges::size(range));
+      statistics.get<counter_t>().set_generic_value(std::ranges::size(range));
     }
+  }
+
+  namespace details {
+    template<typename Fn>
+    using compute_result_t = std::remove_cvref_t<std::invoke_result_t<Fn>>;
+  }
+
+  template<typename Fn>
+  concept simple_computation = std::is_invocable_v<Fn> &&
+      !std::is_same_v<details::compute_result_t<Fn>, void>;
+
+  template<typename Tag, typename Statistics, simple_computation Fn>
+  inline void compute_simple(Statistics& statistics, Fn&& fn) {
+    using value_t = generic_value<details::compute_result_t<Fn>, Tag>;
+
+    if constexpr (tracks_statistic_v<Statistics, value_t>) {
+      statistics.get<value_t>().set_generic_value(
+          std::invoke(std::forward<Fn>(fn)));
+    }
+  }
+
+  template<std::semiregular Value, typename Tag>
+  struct tagged {
+    using tag_t = Tag;
+    using value_t = Value;
+
+    value_t value;
+  };
+
+  namespace details {
+    template<typename Result>
+    struct is_complex_result : std::false_type {};
+
+    template<std::semiregular... Values, typename... Tags>
+    struct is_complex_result<std::tuple<tagged<Values, Tags>...>>
+        : std::true_type {};
+
+    template<typename Result>
+    inline constexpr auto is_complex_result_v =
+        is_complex_result<Result>::value;
+
+    template<typename Statistics, typename Value, typename Tag>
+    inline void unpack_single(Statistics& statistics,
+                              tagged<Value, Tag>&& single) {
+      using value_t = generic_value<Value, Tag>;
+
+      statistics.get<value_t>().set_generic_value(std::move(single.value));
+    }
+
+    template<typename Statistics, typename... Tys>
+    inline void unpack_complex(Statistics& statistics,
+                               std::tuple<Tys...>&& pack) {
+      (unpack_single(statistics, std::move(std::get<Tys>(pack))), ...);
+    }
+  } // namespace details
+
+  template<typename Fn>
+  concept complex_computation = simple_computation<Fn> &&
+      details::is_complex_result_v<details::compute_result_t<Fn>>;
+
+  template<typename Statistics, complex_computation Fn>
+  inline void compute_complex(Statistics& statistics, Fn&& fn) {
+    details::unpack_complex(statistics, std::invoke(std::forward<Fn>(fn)));
   }
 
   template<statistical Statistics>
