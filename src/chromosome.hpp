018f6c5f1cd909859ffabb5f2947b0a4def11837
kataklinger <kataklinger@gmail.com>
2022-10-04 02:01:47 +0200

    Chomosome matcher concept

--- src/chromosome.hpp
+++ src/chromosome.hpp
@@ -4,8 +4,9 @@
 #include <array>
 #include <concepts>
 #include <ranges>
-#include <type_traits>
 #include <vector>
+
+#include "utility.hpp"
 
 namespace gal {
 
@@ -16,6 +17,18 @@ template<typename Chromosome>
 concept range_chromosome =
     chromosome<Chromosome> && std::ranges::bidirectional_range<Chromosome> &&
     std::ranges::sized_range<Chromosome>;
+
+template<typename Operation, typename Chromosome>
+concept matcher =
+    chromosome<Chromosome> &&
+    std::is_invocable_v<
+        Operation,
+        std::add_lvalue_reference_t<std::add_const_t<Chromosome>>,
+        std::add_lvalue_reference_t<std::add_const_t<Chromosome>>> &&
+    util::arithmetic<std::invoke_result_t<
+        Operation,
+        std::add_lvalue_reference_t<std::add_const_t<Chromosome>>,
+        std::add_lvalue_reference_t<std::add_const_t<Chromosome>>>>;
 
 template<range_chromosome Chromosome>
 inline auto draft(Chromosome& target, std::size_t /*unused*/) noexcept {
