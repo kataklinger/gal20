27c236186bb980940f7088f0edffaca5ee07b82c
kataklinger <kataklinger@gmail.com>
2022-10-21 00:48:36 +0200

    Condensing example

--- src/gal20.cpp
+++ src/gal20.cpp
@@ -1,6 +1,4 @@
-﻿// gal20.cpp : Defines the entry point for the application.
-//
-
+﻿
 #include "coupling.hpp"
 #include "criteria.hpp"
 #include "crossover.hpp"
@@ -11,49 +9,24 @@
 #include "selection.hpp"
 #include "soo.hpp"
 
-namespace example {
-
-using random_gen = std::mt19937;
-using chromosome = std::vector<double>;
-
 const std::uniform_real_distribution<> dist{-10.0, 10.0};
-
-class initializator {
-public:
-  inline explicit initializator(random_gen& generator) noexcept
-      : generator_{&generator} {
-  }
-
-  inline auto operator()() const noexcept {
-    return chromosome{dist(*generator_), dist(*generator_)};
-  }
-
-private:
-  random_gen* generator_;
-};
-
-struct evaluator {
-  inline auto operator()(chromosome const& chromosome) const noexcept {
-    return std::pow(chromosome[0] + chromosome[1], 2.0);
-  }
-};
-
-} // namespace example
 
 int main() {
   using namespace gal;
 
-  example::random_gen rng{};
+  std::mt19937 rng{};
   std::stop_token stop{};
 
   config::for_map<soo::algo_config_map>()
       .begin()
       .limit(20)
       .tag()
-      .spawn(example::initializator{rng})
-      .evaluate(example::evaluator{}, std::less{})
+      .spawn([&rng] {
+        return std::vector<double>{dist(rng), dist(rng)};
+      })
+      .evaluate([](auto& c) { return std::pow(c[0] + c[1], 2.0); }, std::less{})
       .reproduce(cross::symmetric_singlepoint{rng},
-                 mutate::make_simple_flip<1>(rng, example::dist))
+                 mutate::make_simple_flip<1>(rng, dist))
       .scale()
       .track<stats::generation,
              stats::extreme_fitness_raw,
