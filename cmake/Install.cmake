4849f848f263cfdb1d6ccb5f0f1dddbdbf25cc18
kataklinger <kataklinger@gmail.com>
2023-10-20 23:14:18 +0200

    Restructuring project

---
+++ cmake/Install.cmake
@@ -0,0 +1,17 @@
+cmake_minimum_required(VERSION 3.26)
+
+include(GNUInstallDirs)
+
+install(
+  TARGETS gal20_lib warnings
+  EXPORT gal20_export
+  FILE_SET gal20_lib_headers DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/gal20)
+
+install(
+  EXPORT gal20_export
+  DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/gal20/cmake
+  NAMESPACE gal::)
+
+install(
+  FILES ./cmake/gal20Config.cmake
+  DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/gal20/cmake)
