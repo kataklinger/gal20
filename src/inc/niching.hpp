05fecf23040797d8aa68379db17ea338f23f3fbb
kataklinger <kataklinger@gmail.com>
2023-10-23 01:41:29 +0200

    Simplifying operation names in niche namespace

--- src/inc/niching.hpp
+++ src/inc/niching.hpp
@@ -4,10 +4,10 @@
 namespace gal {
 namespace niche {
 
-  class sharing {}; // fitness sharing (nsga)
-  class distancing {}; // crowding distance (nsga-ii)
-  class clustering {}; // average linkage (spea)
-  class neighboring {}; // kth nearest neighbor (spea-ii)
+  class share {}; // fitness sharing (nsga)
+  class distance {}; // crowding distance (nsga-ii)
+  class cluster {}; // average linkage (spea)
+  class neighbor {}; // kth nearest neighbor (spea-ii)
   class hypergrid {}; // cell-sharing (pesa, pesa-ii, paes)
   class adaptive_hypergrid {}; // adaptive cell-sharing (rdga)
 
