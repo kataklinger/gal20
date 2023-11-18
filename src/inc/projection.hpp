c47522bea3778d992874f68d6d44c0e15a97cd4a
kataklinger <kataklinger@gmail.com>
2023-11-18 20:09:52 +0100

    Adding more projection concepts

--- src/inc/projection.hpp
+++ src/inc/projection.hpp
@@ -6,11 +6,26 @@
 namespace gal {
 namespace project {
 
+  namespace details {
+
+    template<typename Individual, typename From>
+    concept projectable_from =
+        std::constructible_from<typename Individual::scaled_fitness_t, From> &&
+        std::assignable_from<
+            std::add_lvalue_reference_t<typename Individual::scaled_fitness_t>,
+            From>;
+
+  } // namespace details
+
+  template<typename Individual, typename RankTag, typename From>
+  concept projectable = niched_individual<Individual, RankTag> &&
+                        details::projectable_from<Individual, From>;
+
   // x = f(rank) / (1 - density) (nsga)
   template<typename RankTag>
   class scale {
   public:
-    template<niched_individual<RankTag> Individual>
+    template<projectable<RankTag, double> Individual>
     void operator()(Individual& individual) const {
     }
   };
@@ -19,7 +34,7 @@ namespace project {
   template<typename RankTag>
   class translate {
   public:
-    template<niched_individual<RankTag> Individual>
+    template<projectable<RankTag, double> Individual>
     void operator()(Individual& individual) const {
     }
   };
@@ -28,7 +43,7 @@ namespace project {
   template<typename RankTag>
   class merge {
   public:
-    template<niched_individual<RankTag> Individual>
+    template<projectable<RankTag, std::tuple<double, double>> Individual>
     void operator()(Individual& individual) const {
     }
   };
@@ -37,7 +52,7 @@ namespace project {
   template<typename RankTag>
   class truncate {
   public:
-    template<niched_individual<RankTag> Individual>
+    template<projectable<RankTag, double> Individual>
     void operator()(Individual& individual) const {
     }
   };
@@ -46,7 +61,7 @@ namespace project {
   template<typename RankTag>
   class alternate {
   public:
-    template<niched_individual<RankTag> Individual>
+    template<projectable<RankTag, double> Individual>
     void operator()(Individual& individual) const {
     }
   };
