d08ced565a48b63a531a6b24fe6b42b661248394
kataklinger <kataklinger@gmail.com>
2024-01-12 01:10:55 +0100

    Adding test for ordering of elements in pareto sets

--- tests/pareto/pareto_tests.cpp
+++ tests/pareto/pareto_tests.cpp
@@ -2,30 +2,66 @@
 #include "fitness.hpp"
 #include "pareto.hpp"
 
-#include "gtest/gtest.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
-#include "array"
-#include "vector"
+#include <array>
+#include <vector>
+
+using individual_t = std::array<int, 2>;
+
+template<std::ranges::range R>
+constexpr auto to_vector(R&& r) {
+  auto extracted =
+      r | std::views::transform([](auto const& s) { return s.individual(); });
+
+  using elem_t = std::decay_t<std::ranges::range_value_t<decltype(extracted)>>;
+  return std::vector<elem_t>{std::ranges::begin(extracted),
+                             std::ranges::end(extracted)};
+}
 
 class pareto_sort_tests : public testing::Test {
 protected:
+  inline static constexpr individual_t f1a{0, 0};
+  inline static constexpr individual_t f2a{1, 0};
+  inline static constexpr individual_t f2b{0, 1};
+  inline static constexpr individual_t f3a{1, 1};
+
   void SetUp() override {
-    individuals_.push_back({0, 0});
-    individuals_.push_back({1, 0});
-    individuals_.push_back({0, 1});
-    individuals_.push_back({1, 1});
+    individuals_.push_back(f1a);
+    individuals_.push_back(f2a);
+    individuals_.push_back(f2b);
+    individuals_.push_back(f3a);
   }
 
-  std::vector<std::array<int, 2>> individuals_;
+  std::vector<individual_t> individuals_;
 };
 
 TEST_F(pareto_sort_tests, pareto_front_count) {
   // arrange
-  gal::dominate cmp{std::less{}};
+  gal::dominate cmp{std::greater{}};
 
   // act
   auto sorted = individuals_ | gal::pareto::views::sort(cmp);
 
   // assert
   EXPECT_EQ(std::ranges::distance(sorted), 3);
+}
+
+TEST_F(pareto_sort_tests, pareto_front_ordering) {
+  // arrange
+  gal::dominate cmp{std::greater{}};
+
+  // act
+  auto sorted = individuals_ | gal::pareto::views::sort(cmp);
+
+  // assert
+  auto it = std::ranges::begin(sorted);
+  EXPECT_THAT(to_vector(it->members()), ::testing::ElementsAre(f1a));
+
+  ++it;
+  EXPECT_THAT(to_vector(it->members()),
+              ::testing::UnorderedElementsAre(f2a, f2b));
+  ++it;
+  EXPECT_THAT(to_vector(it->members()), ::testing::ElementsAre(f3a));
 }
