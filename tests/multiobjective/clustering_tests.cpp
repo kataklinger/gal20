d488d765e554de1b2f0600481b7ae3bddbe28532
kataklinger <kataklinger@gmail.com>
2024-01-23 23:23:47 +0100

    Tests for hypergrid clustering operations

--- tests/multiobjective/clustering_tests.cpp
+++ tests/multiobjective/clustering_tests.cpp
@@ -256,4 +256,310 @@ TEST_F(linkage_archive_underflow_full_tests, clusters_labels) {
   }
 }
 
+class hypergird_base_tests : public ::testing::Test {
+protected:
+  void SetUp(std::size_t target_size) {
+    using individual_t = population_t::individual_t;
+    using evaluation_t = individual_t::evaluation_t;
+
+    std::vector<individual_t> individuals{
+        {0, evaluation_t{f1a}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1b}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1c}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1d}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1e}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1f}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1g}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1h}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1i}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1j}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1k}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1l}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2a}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2b}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2c}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2d}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2e}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2f}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2g}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2h}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2i}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2j}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2k}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2l}, tags_t{0, 0, 0}}};
+
+    population_ = std::make_unique<population_t>(
+        cmp_t{}, gal::disabled_comparator{}, target_size, false);
+    population_->insert(individuals);
+
+    pareto_ = gal::rank::level{}(*population_, gal::pareto_preserved_tag);
+  }
+
+  std::unique_ptr<population_t> population_;
+  gal::population_pareto_t<population_t, gal::pareto_preserved_t> pareto_{0};
+};
+
+class hypergird_exact_tests : public hypergird_base_tests {
+protected:
+  void SetUp() override {
+    hypergird_base_tests::SetUp(24);
+  }
+};
+
+TEST_F(hypergird_exact_tests, grouped_clusters_count) {
+  // arrange
+  gal::cluster::hypergrid<fitness_t, 0.5, 0.5> op;
+
+  // act
+  auto clusters = op(*population_, pareto_);
+
+  // assert
+  EXPECT_EQ(clusters.size(), 8);
+}
+
+TEST_F(hypergird_exact_tests, grouped_clusters_content) {
+  // arrange
+  gal::cluster::hypergrid<fitness_t, 0.5, 0.5> op;
+
+  // act
+  auto clusters = op(*population_, pareto_);
+
+  // assert
+  std::vector all(clusters.begin(), clusters.end());
+  gal::cluster_set::cluster cluster1{1, 3};
+  gal::cluster_set::cluster cluster2{2, 3};
+
+  EXPECT_THAT(all, ::testing::Contains(cluster1).Times(4));
+  EXPECT_THAT(all, ::testing::Contains(cluster2).Times(4));
+}
+
+TEST_F(hypergird_exact_tests, grouped_clusters_labels) {
+  // arrange
+  gal::cluster::hypergrid<fitness_t, 0.5, 0.5> op;
+
+  // act
+  auto clusters = op(*population_, pareto_);
+
+  // assert
+  for (std::size_t i = 0; i < 21; i += 3) {
+    EXPECT_EQ(get_cluster_label(*population_, i + 0).index(),
+              get_cluster_label(*population_, i + 1).index());
+    EXPECT_EQ(get_cluster_label(*population_, i + 1).index(),
+              get_cluster_label(*population_, i + 2).index());
+  }
+}
+
+TEST_F(hypergird_exact_tests, unique_clusters_count) {
+  // arrange
+  gal::cluster::hypergrid<fitness_t, 0.05, 0.05> op;
+
+  // act
+  auto clusters = op(*population_, pareto_);
+
+  // assert
+  EXPECT_EQ(clusters.size(), 0);
+}
+
+TEST_F(hypergird_exact_tests, unique_clusters_labels) {
+  // arrange
+  gal::cluster::hypergrid<fitness_t, 0.05, 0.05> op;
+
+  // act
+  auto clusters = op(*population_, pareto_);
+
+  // assert
+  for (std::size_t i = 0; i < 24; ++i) {
+    EXPECT_TRUE(get_cluster_label(*population_, i).is_unique());
+  }
+}
+
+class hypergird_overflow_tests : public hypergird_base_tests {
+protected:
+  void SetUp() override {
+    hypergird_base_tests::SetUp(12);
+  }
+};
+
+TEST_F(hypergird_overflow_tests, unique_clusters_count) {
+  // arrange
+  gal::cluster::hypergrid<fitness_t, 0.05, 0.05> op;
+
+  // act
+  auto clusters = op(*population_, pareto_);
+
+  // assert
+  EXPECT_EQ(clusters.size(), 0);
+}
+
+TEST_F(hypergird_overflow_tests, unique_clusters_labels) {
+  // arrange
+  gal::cluster::hypergrid<fitness_t, 0.05, 0.05> op;
+
+  // act
+  auto clusters = op(*population_, pareto_);
+
+  // assert
+  for (std::size_t i = 0; i < 12; ++i) {
+    EXPECT_TRUE(get_cluster_label(*population_, i).is_unique());
+  }
+
+  for (std::size_t i = 12; i < 24; ++i) {
+    EXPECT_TRUE(get_cluster_label(*population_, i).is_unassigned());
+  }
+}
+
+// ------
+
+class adaptive_hypergird_base_tests : public ::testing::Test {
+protected:
+  void SetUp(std::size_t target_size) {
+    using individual_t = population_t::individual_t;
+    using evaluation_t = individual_t::evaluation_t;
+
+    std::vector<individual_t> individuals{
+        {0, evaluation_t{f1a}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1b}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1c}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1d}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1e}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1f}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1g}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1h}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1i}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1j}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1k}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f1l}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2a}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2b}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2c}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2d}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2e}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2f}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2g}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2h}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2i}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2j}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2k}, tags_t{0, 0, 0}},
+        {0, evaluation_t{f2l}, tags_t{0, 0, 0}}};
+
+    population_ = std::make_unique<population_t>(
+        cmp_t{}, gal::disabled_comparator{}, target_size, false);
+    population_->insert(individuals);
+
+    pareto_ = gal::rank::level{}(*population_, gal::pareto_preserved_tag);
+  }
+
+  std::unique_ptr<population_t> population_;
+  gal::population_pareto_t<population_t, gal::pareto_preserved_t> pareto_{0};
+};
+
+class adaptive_hypergird_exact_tests : public adaptive_hypergird_base_tests {
+protected:
+  void SetUp() override {
+    adaptive_hypergird_base_tests::SetUp(24);
+  }
+};
+
+TEST_F(adaptive_hypergird_exact_tests, grouped_clusters_count) {
+  // arrange
+  gal::cluster::adaptive_hypergrid<6, 6> op;
+
+  // act
+  auto clusters = op(*population_, pareto_);
+
+  // assert
+  EXPECT_EQ(clusters.size(), 8);
+}
+
+TEST_F(adaptive_hypergird_exact_tests, grouped_clusters_content) {
+  // arrange
+  gal::cluster::adaptive_hypergrid<6, 6> op;
+
+  // act
+  auto clusters = op(*population_, pareto_);
+
+  // assert
+  std::vector all(clusters.begin(), clusters.end());
+  gal::cluster_set::cluster cluster1{1, 3};
+  gal::cluster_set::cluster cluster2{2, 3};
+
+  EXPECT_THAT(all, ::testing::Contains(cluster1).Times(4));
+  EXPECT_THAT(all, ::testing::Contains(cluster2).Times(4));
+}
+
+TEST_F(adaptive_hypergird_exact_tests, grouped_clusters_labels) {
+  // arrange
+  gal::cluster::adaptive_hypergrid<6, 6> op;
+
+  // act
+  auto clusters = op(*population_, pareto_);
+
+  // assert
+  for (std::size_t i = 0; i < 21; i += 3) {
+    EXPECT_EQ(get_cluster_label(*population_, i + 0).index(),
+              get_cluster_label(*population_, i + 1).index());
+    EXPECT_EQ(get_cluster_label(*population_, i + 1).index(),
+              get_cluster_label(*population_, i + 2).index());
+  }
+}
+
+TEST_F(adaptive_hypergird_exact_tests, unique_clusters_count) {
+  // arrange
+  gal::cluster::adaptive_hypergrid<60, 60> op;
+
+  // act
+  auto clusters = op(*population_, pareto_);
+
+  // assert
+  EXPECT_EQ(clusters.size(), 0);
+}
+
+TEST_F(adaptive_hypergird_exact_tests, unique_clusters_labels) {
+  // arrange
+  gal::cluster::adaptive_hypergrid<60, 60> op;
+
+  // act
+  auto clusters = op(*population_, pareto_);
+
+  // assert
+  for (std::size_t i = 0; i < 24; ++i) {
+    EXPECT_TRUE(get_cluster_label(*population_, i).is_unique());
+  }
+}
+
+class adaptive_hypergird_overflow_tests : public adaptive_hypergird_base_tests {
+protected:
+  void SetUp() override {
+    adaptive_hypergird_base_tests::SetUp(12);
+  }
+};
+
+TEST_F(adaptive_hypergird_overflow_tests, unique_clusters_count) {
+  // arrange
+  gal::cluster::adaptive_hypergrid<60, 60> op;
+
+  // act
+  auto clusters = op(*population_, pareto_);
+
+  // assert
+  EXPECT_EQ(clusters.size(), 0);
+}
+
+TEST_F(adaptive_hypergird_overflow_tests, unique_clusters_labels) {
+  // arrange
+  gal::cluster::adaptive_hypergrid<60, 60> op;
+
+  // act
+  auto clusters = op(*population_, pareto_);
+
+  // assert
+  for (std::size_t i = 0; i < 12; ++i) {
+    EXPECT_TRUE(get_cluster_label(*population_, i).is_unique());
+  }
+
+  for (std::size_t i = 12; i < 24; ++i) {
+    EXPECT_TRUE(get_cluster_label(*population_, i).is_unassigned());
+  }
+}
+
 } // namespace tests::clustering
